625af90158164539d9f8545a1e5b264247219490
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-05-25 17:27:59 +0000

    operatorsdone

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,15 @@
+//let number =3;
+//let negvalue = -number;
+//console.log(negvalue)
+
+//let str1= "Obaid";
+//let str2= "ullah";
+//let str3 = str1 + str2;
+//console.log(str3);
+
+//console.log("3" + 2);
+//console.log("3" + 2+ 5);
+//console.log(3 + 2);
+console.log(true);
+console.log(+true);
+//console.log(3 + 2+ "5");
