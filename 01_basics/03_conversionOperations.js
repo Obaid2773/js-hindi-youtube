aa630fa460313f4405e815af7a88666b1c925961
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-05-25 15:46:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = undefined 
+
+console.log(typeof score);
+
+let valueNumber = Number(score);
+console.log(valueNumber);
+
+let isLoggedin = "kwudh";  // ""=> false . ",efh"=>true
+let convBool = Boolean(isLoggedin)
+console.log(convBool)
+
+let number = 33;
+let strngnum = String(number)
+console.log(strngnum);
+console.log(typeof strngnum);
