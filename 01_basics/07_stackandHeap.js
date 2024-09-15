4d38f4dceb6f0be27be74eb6e9c8c590abdcb1e2
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-15 09:12:27 +0000

    recapDone

--- 01_basics/07_stackandHeap.js
+++ 01_basics/07_stackandHeap.js
@@ -5,8 +5,8 @@
 let name1 = "Obaidullah"
 let yourname = name1
 yourname = "Fehad";
-console.log(yourname);
-console.log(name1);
+// console.log(yourname);
+// console.log(name1);
 
 //eg of heap
 
@@ -19,4 +19,23 @@ let obj1 = {
 let obj2 = obj1;
  obj2.email = "Zahroor@hotmail.com";
 
- console.log(obj1.email);
+//  console.log(obj1.email);
+
+
+ let naav = "Fehad Nazir"
+ let FehadsName = naav
+
+ FehadsName = "Fahad"
+
+ console.log(naav);
+ console.log(FehadsName);
+
+ let UserOne = {
+    name: "Obaid",
+    email: "Dengmospta@ladakh.com",
+ }
+ let userTwo = UserOne;
+ userTwo.email= "Darling.com";
+
+ console.log(UserOne.email);
+ 
