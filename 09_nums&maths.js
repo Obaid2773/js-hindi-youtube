7527a9e71cb8695d78eadc0687c95ead4630df85
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-16 09:31:58 +0000

    numandmathsdone

---
+++ 09_nums&maths.js
@@ -0,0 +1,49 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(typeof(score));
+// console.log(typeof(balance));
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(score.toFixed(2));
+
+
+const num = 12345.658
+// console.log(num.toPrecision(5));
+
+const Currency = 100000;
+// console.log(Currency.toLocaleString('en-IN')); // INDIAN
+
+// -------************MATHS*************------------
+
+
+
+const num1 = -4;
+// console.log(Math.abs(-89));
+// console.log(Math.ceil(4.235 ));
+// console.log(Math.floor(4.735 ));
+// console.log(Math.max(1,2,3,4,5,6,7,8,9));
+// console.log(Math.min(1,2,3,4,5,6,7,8,9));
+
+// most important = Math.rand ---->>>Random
+
+// console.log(Math.random()); // 0&1 k between deta hai values
+
+// suppose 4 digits a rand chahiye 0-9999 tak
+
+const min = 1000;
+const max = 9999;
+
+console.log(Math.floor(Math.random()* (max-min) + min));
+
+
+
+
+
+
+
+
