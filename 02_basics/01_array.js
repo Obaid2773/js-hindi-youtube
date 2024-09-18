8a9161842d2a03b27cea5ce39b2b36480ab938f2
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-18 10:18:09 +0000

    arraysdone

---
+++ 02_basics/01_array.js
@@ -0,0 +1,37 @@
+// const numbers = [1,2,3,4,5];
+// console.log(numbers);
+// const nums = new Array(1,2,3,4,5)
+// console.log(nums);
+// console.log(nums.includes(3));
+// console.log(nums.indexOf(5));
+// nums.push(6);
+// console.log(nums);
+// nums.pop()
+// console.log(nums);
+// console.log(nums.includes(8));
+// console.log(nums.indexOf(18));
+
+// // nums.unshift(14)
+// // console.log(nums);
+// // nums.shift()
+// // console.log(nums);
+// // nums.shift()
+// // console.log(nums);
+// console.log(nums.slice(1,3));
+// console.log(nums);
+
+// const arr = [5,6,7,8,9]
+// console.log(arr.slice(0,2));
+// console.log(arr.splice(0,3));
+// const arr1 = arr.splice(0,2);
+// console.log(arr1);
+
+
+
+
+const array = [1,2,3,4,5,6,7,8,9]
+array.splice(1,3)
+console.log(array);
+
+ 
+
