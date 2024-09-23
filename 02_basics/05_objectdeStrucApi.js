043ba9f96b58541a84d6bbe42ae38571299667e4
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-23 06:21:32 +0000

    objst&apibscdone

---
+++ 02_basics/05_objectdeStrucApi.js
@@ -0,0 +1,24 @@
+const course = {
+    courseName: 'Js in Hindi',
+    CoursePrice: '999',
+    CourseInstructor: 'HiteshChoudhary'
+}
+console.log(course.CourseInstructor)
+
+const {CourseInstructor: Instructor} = course;
+console.log(Instructor);
+
+// ==============API====================
+// {
+//     'name': 'obaid',
+//     'age': 22,
+//     'color': 'indian'
+// }
+
+[
+    {},
+    {},
+    {},
+    {},
+    {},
+]
