302fdd8a99ea71c86a66fe607288314f8dce82eb
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-25 08:46:35 +0000

    funD

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,23 @@
+// function CalcCartPrice(...nos){
+//     return nos
+// }
+
+// console.log(CalcCartPrice(4,2,3,5,7));
+
+// const objjj = {
+//     username: 'Obaid',
+//     price: '₹900'
+
+// }
+// function getObj(anyobject){
+// return `the username is ${anyobject.username} and its price is ₹ ${anyobject.price}`
+
+
+// console.log(getObj(objjj));
+const arr =[1,2,3,4,5,6,7]
+
+function getArr(array){
+    console.log(array[1]);
+    
+}
+getArr([100,200,300,400,500,600,700,800,900,1000]);
