042ff0cdc9fc33169b64fb0108612ade502bd7dd
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-28 09:09:06 +0000

    iifeefunDone

---
+++ 03_basics/IIFEE.js
@@ -0,0 +1,9 @@
+// iifee functions
+
+((num1) =>
+
+console.log('Hello world', num1)
+)
+(43)
+//--------------iifee functions done--------------
+// usi waqat implement karne walla function hota hai ifee function
