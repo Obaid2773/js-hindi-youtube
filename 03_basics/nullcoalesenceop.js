e384c250b25e44b74a7f6365c59d585e243c9c24
Ubaidullah <ubaidullah_2020bece009@nitsri.ac.in>
2024-09-30 09:51:02 +0000

    nullcoalenseandternary

---
+++ 03_basics/nullcoalesenceop.js
@@ -0,0 +1,14 @@
+//  null coalesence operator ?? null & undefined
+
+let val1 = 10 ?? 26
+val1 = null ?? 0
+console.log(val1);
+
+val1 = undefined ?? 0
+console.log(val1);
+//  Ternary Operator
+
+// condition ? true : false
+let price = 100
+price > 50 ? console.log('Expensive') : console.log('take It');
+
